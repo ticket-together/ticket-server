f0d3b4d86c519537774b1b51ca69532f6743c0ec
limjimin <dkdldsla0000@naver.com>
2022-03-19 17:39:13 +0900

    init

---
+++ src/main/java/com/java/tickettogether/TickettogetherApplication.java
@@ -0,0 +1,13 @@
+package com.java.tickettogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickettogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickettogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/tickettogether/TickettogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.tickettogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickettogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
