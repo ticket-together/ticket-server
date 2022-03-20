af9faf6d73c02e20de299067bcd90b62e03b0154
limjimin <dkdldsla0000@naver.com>
2022-03-20 15:49:10 +0900

    :wrench: 프로젝트 기본 세팅 (#1)

--- src/main/java/com/tickettogether/TickettogetherApplication.java
+++ src/main/java/com/tickettogether/TickettogetherApplication.java
@@ -1,4 +1,4 @@
-package com.java.tickettogether;
+package com.tickettogether;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
