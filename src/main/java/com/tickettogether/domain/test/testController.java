2161433abacde2fb4797f3e98b9b6d90b02469fc
limjimin <dkdldsla0000@naver.com>
2022-04-11 19:33:42 +0900

    :construction_worker: CI, docker hub에 push (#16)

--- src/main/java/com/tickettogether/domain/test/testController.java
+++ src/main/java/com/tickettogether/domain/test/testController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @RequestMapping("/test")
     public String sample(){
-        return "ticket-together";
+        return "ticket-together, deploy check";
     }
 }
