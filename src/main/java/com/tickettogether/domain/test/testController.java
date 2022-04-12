00eb2bda6bbb1444f2a453572235248278f4ff5d
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2022-04-12 18:33:32 +0900

    Feature/cicd (#21)
    * :lock: secret 코드 관련 수정 (#16)
    
    * :wrench: Dockerfile 추가 (#16)
    
    * :construction_worker: CI, docker hub에 push (#16)
    
    * :green_heart: CI 수정 (#16)
    
    * :rocket: CD 설정 추가 (#16)
    
    * :poop: CD 테스트 (#16)
    
    * 🔧 workflows branch 수정
    
    * 🔧 workflows branch 수정

---
+++ src/main/java/com/tickettogether/domain/test/testController.java
@@ -0,0 +1,12 @@
+package com.tickettogether.domain.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @RequestMapping("/test")
+    public String sample(){
+        return "ticket together deploy 확인입니당.";
+    }
+}
