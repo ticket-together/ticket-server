e9721779ff04dfbc1d875b4649ea6c69e0cca29b
limjimin <dkdldsla0000@naver.com>
2022-04-14 00:55:29 +0900

    :sparkles: 캘린더 수정, 삭제 기능 추가 (#9)

--- src/main/java/com/tickettogether/global/common/Constant.java
+++ src/main/java/com/tickettogether/global/common/Constant.java
@@ -2,5 +2,5 @@ package com.tickettogether.global.common;
 
 // 프로젝트에서 공통적으로 사용하는 상수들
 public class Constant {
-    // 예시 public static final String IP_ADDRESS = "127.0.0.1";
+    public static final String CATEGORY_CALENDAR = "calendar";
 }
