73a42708a4c61f4176eea56a5fe10e64aae51e95
limjimin <dkdldsla0000@naver.com>
2022-05-13 02:34:10 +0900

    :white_check_mark: redis test

--- src/main/java/com/tickettogether/domain/test/testController.java
+++ src/main/java/com/tickettogether/domain/test/testController.java
@@ -1,12 +1,16 @@
 package com.tickettogether.domain.test;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class testController {
+    private final TestService testService;
     @RequestMapping("/test")
     public String sample(){
+        testService.redisString();
         return "ticket together deploy 확인입니당.";
     }
 }
