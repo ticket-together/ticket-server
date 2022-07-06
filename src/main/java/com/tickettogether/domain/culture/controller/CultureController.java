173e7a6550c4ea8f248077d800dd8d61b2f14e59
limjimin <dkdldsla0000@naver.com>
2022-07-06 21:25:31 +0900

    :sparkles: 공연 정보 검색 controller

--- src/main/java/com/tickettogether/domain/culture/controller/CultureController.java
+++ src/main/java/com/tickettogether/domain/culture/controller/CultureController.java
@@ -1,16 +1,15 @@
 package com.tickettogether.domain.culture.controller;
-
+;
 import com.tickettogether.domain.culture.dto.CultureDto;
 import com.tickettogether.domain.culture.dto.CultureResponseMessage;
 import com.tickettogether.domain.culture.service.CultureService;
 import com.tickettogether.global.error.dto.BaseResponse;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,7 +20,14 @@ public class CultureController {
 
     @ApiOperation(value = "공연 상세 조회")
     @GetMapping("/{id}")
-    public ResponseEntity<BaseResponse<CultureDto.CultureResponse>> getCulture(@PathVariable("id") Long id){
-        return ResponseEntity.ok(BaseResponse.create(CultureResponseMessage.GET_CALENDARS_SUCCESS.getMessage(), cultureService.getCulture(id)));
+    public ResponseEntity<BaseResponse<CultureDto.CultureResponse>> getCulture(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(BaseResponse.create(CultureResponseMessage.GET_CULTURE_SUCCESS.getMessage(), cultureService.getCulture(id)));
+    }
+
+    @ApiOperation(value = "공연 검색")
+    @GetMapping("/search")
+    public ResponseEntity<BaseResponse<CultureDto.CultureSearchResponse>> searchCulture(@RequestParam(value = "query", defaultValue = "") String query,
+                                                                                     @PageableDefault(size = 10) Pageable pageable) {
+        return ResponseEntity.ok(BaseResponse.create(CultureResponseMessage.GET_CULTURE_SUCCESS.getMessage(), cultureService.searchCulture(query, pageable)));
     }
 }
