8cb6715ce1193cdc2e04f1728ac54d11ec1ef4c0
semi-cloud <sksmsrkdtpal@naver.com>
2022-07-08 18:27:49 +0900

    :recycle: Member 컨트롤러 Swagger 적용 (#59)

--- src/main/java/com/tickettogether/domain/member/controller/MemberController.java
+++ src/main/java/com/tickettogether/domain/member/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.tickettogether.domain.member.controller;
 import com.tickettogether.domain.member.dto.MemberDto;
 import com.tickettogether.domain.member.service.MemberServiceImpl;
 import com.tickettogether.global.error.dto.BaseResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,18 +21,17 @@ public class MemberController {
     private final MemberServiceImpl memberService;
     private Long tempMemberId = 1L;
 
-    /**
-     * 회원 정보 저장
-     */
+    @ApiOperation(value = "회원 정보 저장", notes = "카카오 로그인 후 입력 받은 정보를 가지고 회원 정보를 마저 저장한다.")
+    @ApiResponse(code = 2012, message = "존재하지 않는 키워드입니다.")
     @PostMapping
     @ResponseBody
     public ResponseEntity<BaseResponse<MemberDto.SaveResponse>> saveMemberInfo(@RequestBody MemberDto.SaveRequest saveRequest){
         return ResponseEntity.ok(BaseResponse.create(SAVE_MEMBER_SUCCESS.getMessage(),
                 memberService.saveMemberProfile(saveRequest, tempMemberId)));
     }
-    /**
-     회원 정보 조회
-     */
+
+    @ApiOperation(value = "본인 정보 조회", notes = "로그인 유저의 정보를 조회한다.")
+    @ApiResponse(code = 2011, message = "존재하지 않는 회원입니다.")
     @GetMapping
     @ResponseBody
     public ResponseEntity<BaseResponse<MemberDto.SearchResponse>> searchMemberInfo(){
@@ -38,9 +39,8 @@ public class MemberController {
                 memberService.getMemberProfile(tempMemberId)));
     }
 
-    /**
-     다른 회원 정보 조회
-     */
+    @ApiOperation(value = "다른 회원 정보 조회", notes = "로그인 유저가 아닌 다른 회원의 정보를 조회한다.")
+    @ApiResponse(code = 2011, message = "존재하지 않는 회원입니다.")
     @GetMapping("/{memberId}")
     @ResponseBody
     public ResponseEntity<BaseResponse<MemberDto.SearchResponse>> searchMemberInfo(@PathVariable("memberId") Long memberId){
@@ -48,9 +48,8 @@ public class MemberController {
                 memberService.getOtherMemberProfile(memberId)));
     }
 
-    /**
-     회원 정보 수정
-     */
+    @ApiOperation(value = "회원 정보 수정", notes = "로그인 유저의 정보를 수정한다.")
+    @ApiResponse(code = 2011, message = "존재하지 않는 회원입니다.")
     @PatchMapping
     public ResponseEntity<BaseResponse<MemberDto.UpdateResponse>> updateMemberInfo(@RequestBody MemberDto.UpdateRequest updateRequest){
         return ResponseEntity.ok(BaseResponse.create(UPDATE_PROFILE_SUCCESS.getMessage(),
