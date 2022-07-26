03ffbbf43260955f9bcf566c7c1088f3fb139e50
semi-cloud <sksmsrkdtpal@naver.com>
2022-07-26 17:48:03 +0900

    :recycle: 웹소켓 설정 정보 수정 (#81)

--- src/main/java/com/tickettogether/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/tickettogether/domain/chat/config/WebSocketConfig.java
@@ -3,23 +3,25 @@ package com.tickettogether.domain.chat.config;
 import com.tickettogether.domain.chat.controller.WebSocketHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
-@EnableWebSocket
+@EnableWebSocketMessageBroker
 @Configuration
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final WebSocketHandler webSocketHandler;
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/room")
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
                 .setAllowedOrigins("*")
                 .withSockJS();
+    }
 
-        registry.addHandler(webSocketHandler, "/room")
-                .setAllowedOrigins("*");
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
     }
 }
