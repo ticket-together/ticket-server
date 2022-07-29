517c09eab9eb7088d656ff0492c4a44c6b8b5cc5
Semi kang <71436576+semi-cloud@users.noreply.github.com>
2022-07-29 13:52:09 +0900

    :bug: 프로퍼티 소스 추가 (#85)

--- src/main/java/com/tickettogether/global/config/security/jwt/JwtConfig.java
+++ src/main/java/com/tickettogether/global/config/security/jwt/JwtConfig.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application-dev.yml")
 @RequiredArgsConstructor
 public class JwtConfig {
     @Value("${jwt.secret}")
