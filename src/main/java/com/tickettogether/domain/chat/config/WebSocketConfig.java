5cdfff25d0e4e859b457b5f9d120c3ca07a74a9c
limjimin <dkdldsla0000@naver.com>
2022-08-21 05:05:00 +0900

    :wrench: relay host 수정

--- src/main/java/com/tickettogether/domain/chat/config/WebSocketConfig.java
+++ src/main/java/com/tickettogether/domain/chat/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.tickettogether.domain.chat.config;
 
 import com.tickettogether.domain.chat.CustomWebSocketHandlerDecorator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.util.AntPathMatcher;
@@ -9,6 +10,9 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 @Configuration
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${spring.rabbitmq.host}")
+    private String host;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -24,7 +28,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setPathMatcher(new AntPathMatcher("."));   // topic exchange
         registry.setApplicationDestinationPrefixes("/pub");
-        registry.enableStompBrokerRelay("/topic");
+        registry.enableStompBrokerRelay("/topic")
+                .setRelayHost(host)
+                .setRelayPort(61613);
     }
 
     @Override
