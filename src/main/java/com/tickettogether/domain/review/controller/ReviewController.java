5b79b4a3737327fdc76573d36b9162b857b4d13e
limjimin <dkdldsla0000@naver.com>
2022-08-26 02:00:29 +0900

    :adhesive_bandage: 필요없는 주석 삭제 및 swagger 작성

--- src/main/java/com/tickettogether/domain/review/controller/ReviewController.java
+++ src/main/java/com/tickettogether/domain/review/controller/ReviewController.java
@@ -5,12 +5,11 @@ import com.tickettogether.domain.review.dto.ReviewSearchCondition;
 import com.tickettogether.domain.review.dto.ReviewInfoDto;
 import com.tickettogether.domain.review.service.ReviewService;
 import com.tickettogether.global.error.dto.BaseResponse;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 import static com.tickettogether.domain.review.dto.ReviewResponseMessage.*;
 
 @RestController
@@ -19,23 +18,21 @@ import static com.tickettogether.domain.review.dto.ReviewResponseMessage.*;
 public class ReviewController {
 
     private final ReviewService reviewService;
-
     private Long memberId = 1L;  // 테스트 용
 
-    // 리뷰 작성,저장
+    @ApiOperation(value = "리뷰 작성")
     @PostMapping("/add")
     public ResponseEntity<BaseResponse<ReviewDto.addResponse>> addReview(@RequestParam("hall") String hallName, @RequestBody ReviewDto.addRequest reviewDto){
         return ResponseEntity.ok(BaseResponse.create(SAVE_REVIEW_SUCCESS.getMessage(),reviewService.addReview(memberId, hallName, reviewDto)));
     }
 
-
-    // 리뷰 조회 페이지 (해당 공연장, 좌석)
+    @ApiOperation(value = "리뷰 조회", notes = "공연장 이름, 좌석으로 조회")
     @GetMapping("/search")
     public ResponseEntity<BaseResponse<List<ReviewInfoDto>>> searchReviews(@RequestParam("hall") String hallName, ReviewSearchCondition condition) {
         return ResponseEntity.ok(BaseResponse.create(GET_REVIEW_SUCCESS.getMessage(),reviewService.searchReviewBySeat(hallName, condition)));
     }
 
-    // 리뷰 수정
+    @ApiOperation(value = "리뷰 수정")
     @PutMapping("/update")
     public ResponseEntity<BaseResponse<String>> updateReview(@RequestParam("hall") String hallName,
                                                              @RequestParam("reviewId") Long reviewId,
@@ -44,13 +41,12 @@ public class ReviewController {
         return ResponseEntity.ok(BaseResponse.create(MODIFY_REVIEW_SUCCESS.getMessage()));
     }
 
-    // 리뷰 삭제
+    @ApiOperation(value = "리뷰 삭제")
     @DeleteMapping("/delete")
-    public ResponseEntity<BaseResponse<String>> deleteReview(@RequestParam("hall") String hallName, @RequestParam("reviewId") Long reviewId){
+    public ResponseEntity<BaseResponse<String>> deleteReview(@RequestParam("reviewId") Long reviewId){
         reviewService.deleteReview(reviewId);
         return ResponseEntity.ok(BaseResponse.create(DELETE_REVIEW_SUCCESS.getMessage()));
     }
-
 }
 
 
