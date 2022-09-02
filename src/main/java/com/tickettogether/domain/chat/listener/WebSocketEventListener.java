6eec952b53ccc23efef862404718e67e9fc42cd0
semi-cloud <sksmsrkdtpal@naver.com>
2022-09-02 23:05:12 +0900

    :bug: 채팅방 나갈때 발생하는 에러 해결 (#109)

--- src/main/java/com/tickettogether/domain/chat/listener/WebSocketEventListener.java
+++ src/main/java/com/tickettogether/domain/chat/listener/WebSocketEventListener.java
@@ -1,6 +1,7 @@
 package com.tickettogether.domain.chat.listener;
 
 
+import com.tickettogether.global.config.redis.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -11,6 +12,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import java.util.Map;
 import java.util.Objects;
 
 import static com.tickettogether.domain.chat.domain.ChatMessage.*;
@@ -23,10 +25,11 @@ public class WebSocketEventListener {
 
     private final RabbitTemplate rabbitTemplate;
 
+    private final RedisUtil<String, String> redisUtil;
+
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-
         String sessionId = headerAccessor.getSessionId();
         log.info("Received a new web socket connection from Session Id :  {}", sessionId);
     }
@@ -38,10 +41,10 @@ public class WebSocketEventListener {
         String sessionId = headerAccessor.getSessionId();
         String username = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("username");
         String roomId = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("roomId");
+        log.info("User Disconnected : {} ( Session ID : {})", username, sessionId);
 
-        if (StringUtils.hasText(username)){
-            log.info("User Disconnected : {} ( Session ID : {})", username, sessionId);
-
+        Map<String, String> enterUserList = redisUtil.getHashKeys(roomId);
+        if(StringUtils.hasText(username) && !enterUserList.containsKey(username)){
             ChatMessageResponse disconnectMessage = ChatMessageResponse.builder()
                     .type(MessageType.LEAVE.name())
                     .data(username + "님이 나갔습니다.")
