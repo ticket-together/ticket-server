477d1a4a34c0c31b8ab9586f77ed948d6f319710
limjimin <dkdldsla0000@naver.com>
2022-09-12 19:24:37 +0900

    :adhesive_bandage: import문 정리

--- src/main/java/com/tickettogether/domain/member/repository/MemberRepositoryImpl.java
+++ src/main/java/com/tickettogether/domain/member/repository/MemberRepositoryImpl.java
@@ -1,16 +1,9 @@
 package com.tickettogether.domain.member.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.tickettogether.domain.culture.domain.QCulture;
 import com.tickettogether.domain.member.domain.Member;
-import com.tickettogether.domain.member.domain.QMember;
-import com.tickettogether.domain.parts.domain.MemberParts;
-import com.tickettogether.domain.parts.domain.QMemberParts;
-import com.tickettogether.domain.parts.domain.QParts;
 import lombok.RequiredArgsConstructor;
-
 import java.util.Optional;
-
 import static com.tickettogether.domain.culture.domain.QCulture.culture;
 import static com.tickettogether.domain.member.domain.QMember.member;
 import static com.tickettogether.domain.parts.domain.QMemberParts.memberParts;
