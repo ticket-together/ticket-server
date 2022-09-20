230572418ad83abbd87d5c71358fd7a0814d2804
semi-cloud <sksmsrkdtpal@naver.com>
2022-09-20 11:59:29 +0900

    :bug: Cors Filter 에러 해결 (#175)

--- src/main/java/com/tickettogether/global/config/web/WebMvcConfig.java
+++ src/main/java/com/tickettogether/global/config/web/WebMvcConfig.java
@@ -1,15 +1,23 @@
 package com.tickettogether.global.config.web;
 
 import com.tickettogether.global.config.security.annotation.CustomArgumentResolver;
+import com.tickettogether.global.config.security.oauth.dto.CorsProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
 
     @Bean
     public CustomArgumentResolver customArgumentResolver(){
@@ -19,5 +27,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(customArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(corsProperties.getAllowedOrigins().split(","))
+                .allowedMethods(corsProperties.getAllowedMethods().split(","))
+                .allowedHeaders(corsProperties.getAllowedHeaders().split(","))
+                .allowCredentials(true)
+                .maxAge(corsProperties.getMaxAge());
     }
 }
