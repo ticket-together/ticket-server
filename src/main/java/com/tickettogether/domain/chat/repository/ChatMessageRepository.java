4384c29fc2d9bbd515c5bfa745f2a5e536ffbcd5
semi-cloud <sksmsrkdtpal@naver.com>
2022-09-22 00:00:14 +0900

    :bug: 페이징 순서 역순으로 (#128)

--- src/main/java/com/tickettogether/domain/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/tickettogether/domain/chat/repository/ChatMessageRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    Slice<ChatMessage> findAllByChatRoomAndCreatedAtGreaterThanEqualOrderByCreatedAt(Pageable pageable, ChatRoom chatRoom, LocalDateTime createdAt);
+    Slice<ChatMessage> findAllByChatRoomAndCreatedAtGreaterThanEqualOrderByCreatedAtDesc(Pageable pageable, ChatRoom chatRoom, LocalDateTime createdAt);
 }
