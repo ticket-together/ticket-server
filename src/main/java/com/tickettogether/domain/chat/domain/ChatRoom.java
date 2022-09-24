5d1915a7e5ebbad2a4014d3dfb6c384fc68f7d3c
semi-cloud <sksmsrkdtpal@naver.com>
2022-09-24 19:39:44 +0900

    :adhesive_bandage: 채팅룸 엔티티 멤버 필드로 수정 (#179)

--- src/main/java/com/tickettogether/domain/chat/domain/ChatRoom.java
+++ src/main/java/com/tickettogether/domain/chat/domain/ChatRoom.java
@@ -1,5 +1,6 @@
 package com.tickettogether.domain.chat.domain;
 
+import com.tickettogether.domain.member.domain.Member;
 import com.tickettogether.domain.parts.domain.Parts;
 import lombok.*;
 import org.hibernate.mapping.Join;
@@ -9,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -27,11 +27,16 @@ public class ChatRoom {
     @JoinColumn(name = "parts_id")
     private Parts parts;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
     private String name;
 
     @Builder
-    public ChatRoom(String name, Parts parts){
+    public ChatRoom(String name, Parts parts, Member member){
         this.name = name;
         this.parts = parts;
+        this.member = member;
     }
 }
