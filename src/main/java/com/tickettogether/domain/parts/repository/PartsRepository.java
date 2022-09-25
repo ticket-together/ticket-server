4e98623924a8ef8dda9759d731c196d1abdaa6c8
younghyun-na <younghyunna@naver.com>
2022-09-25 18:13:42 +0900

    :sparkles: 팟 status 기준 정렬 기능 추가 (#184)

--- src/main/java/com/tickettogether/domain/parts/repository/PartsRepository.java
+++ src/main/java/com/tickettogether/domain/parts/repository/PartsRepository.java
@@ -2,10 +2,12 @@ package com.tickettogether.domain.parts.repository;
 
 import com.tickettogether.domain.culture.domain.Culture;
 import com.tickettogether.domain.parts.domain.Parts;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PartsRepository extends JpaRepository<Parts, Long>{
-    List<Parts> findByCultureOrderByPartDate(Culture culture);
+
+    List<Parts> findByCulture(Culture culture, Sort sort);
 }
